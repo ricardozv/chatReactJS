435382024a51d7cd0327a93dfd1ce0406ecb107e
ricardozv <ricardosilvanet03@gmail.com>
2020-08-15 23:23:12 -0300

    yes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   <Router>
@@ -13,4 +13,3 @@ ReactDOM.render(
 );
 
 
-serviceWorker.unregister();
